568752e3dd912b41943078f8f0d1be361cc64f9e
Michael Koby <michael@mkoby.com>
2011-05-28 15:56:05 -0500

    Initial Rails app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheNoows::Application.config.secret_token = 'b478ea2050eb458d088b462253b9b0a641ebe93323046b7e747b5586325b3f682d5b97439d1f66836384a4e94b1e831743975faf5822e4b3fd5d143d88eeea11'
