404355da5f2815e0a13e5dbee1619a1c7d6f3cc9
Michael Koby <michael@mkoby.com>
2011-06-04 12:18:59 -0500

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-@top_news_category = Factory(:source_category)
-Factory(:source, :source_category_id => @top_news_category.id)
+@top_news_category = SourceCategory.create!(:name => 'Latest News', :description => 'The latest news headlines')
+Source.create!(:source_category_id => @top_news_category.id, :name => "CNN Top Stories", :homepage_url => 'http://www.cnn.com', :rss_url => 'http://rss.cnn.com/rss/cnn_topstories.rss')
 Source.create!(:source_category_id => @top_news_category.id, :name => "Fox News Latest Headlines", :homepage_url => 'http://www.foxnews.com', :rss_url => 'http://feeds.foxnews.com/foxnews/latest')
 Source.create!(:source_category_id => @top_news_category.id, :name => "MSNBC Top Headlines", :homepage_url => 'http://www.msnbc.com', :rss_url => 'http://pheedo.msnbc.msn.com/id/3032091/device/rss')
