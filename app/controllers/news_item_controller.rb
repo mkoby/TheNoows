1cfe48079cf72c85bd5c4e3508de0b7ab2f38644
Michael Koby <michael@mkoby.com>
2011-06-04 12:21:34 -0500

    added id to news_items group_by

--- app/controllers/news_item_controller.rb
+++ app/controllers/news_item_controller.rb
@@ -1,6 +1,6 @@
 class NewsItemController < ApplicationController
   def list
-    @items = NewsItem.paginate( :page => params[:page], :per_page => 50, :order => 'published_at DESC', :group => 'news_items.published_at, news_items.link, news_items.title, news_items.total_votes, news_items.created_at, news_items.updated_at' )
+    @items = NewsItem.paginate( :page => params[:page], :per_page => 50, :order => 'published_at DESC', :group => 'news_items.published_at, news_items.link, news_items.title, news_items.total_votes, news_items.created_at, news_items.updated_at, news_items.id' )
   end
 
   def vote_up
