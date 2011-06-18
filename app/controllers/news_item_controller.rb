c165ab1b502c3d3345c0a5dd4a0dae97c9a07251
Michael Koby <michael@mkoby.com>
2011-06-18 14:37:43 -0500

    Added main layout to get css Noows buttons into site

--- app/controllers/news_item_controller.rb
+++ app/controllers/news_item_controller.rb
@@ -1,6 +1,16 @@
 class NewsItemController < ApplicationController
+  layout 'main'
+
   def list
-    @items = NewsItem.paginate( :page => params[:page], :per_page => 50, :order => 'published_at DESC', :group => 'news_items.published_at, news_items.link, news_items.title, news_items.total_votes, news_items.created_at, news_items.updated_at, news_items.id, news_items.source_id' )
+    @items = NewsItem.paginate( :page => params[:page], :per_page => 50, 
+                                :order => 'published_at DESC', 
+                                :group => 'news_items.published_at, news_items.link, news_items.title, 
+                                           news_items.total_votes, news_items.created_at, 
+                                           news_items.updated_at, news_items.id, news_items.source_id' )
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def vote_up
